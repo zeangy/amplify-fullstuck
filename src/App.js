dddaa98b9db9af40051cebb743e7db41fbd7b85f
Angelo Malimi <angelo-malimi@Angelo-Malimis-MacBook-Pro.local>
2024-01-17 12:09:46 -0800

    word-changing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        test to this if this work
         </a>
       </header>
     </div>
